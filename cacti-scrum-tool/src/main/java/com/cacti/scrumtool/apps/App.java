9816c755c789969a29fcc435e3895f21dbaac96f
zwu <zwu@localhost>
2013-09-24 21:45:14 -0400

    Init Project, testing Jenkins and Sonar

---
+++ cacti-scrum-tool/src/main/java/com/cacti/scrumtool/apps/App.java
@@ -0,0 +1,24 @@
+package com.cacti.scrumtool.apps;
+
+public class App {
+	
+	public static void  printTest(){
+		System.out.println("Never called method");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test My Maven Project");
+		
+		//Dead Code
+		if(true){
+			
+		}
+		
+		System.out.println("Dead Code");
+	}
+
+}
